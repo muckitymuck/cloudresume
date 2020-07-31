cbc163f77f881756227d148f7d78df323fbc4998
muckity <andrewgoldstein@hotmail.com>
2020-07-31 06:53:13 -0500

    script.js update

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-var request = new XMLHttpRequest()
+var request = new XMLHttpRequest();
 
-request.open('GET', 'https://4lnixszdo4.execute-api.us-east-2.amazonaws.com/test/visitcount', true)
-
+request.open('GET', 'https://4lnixszdo4.execute-api.us-east-2.amazonaws.com/test/visitcount', true);
+                    
 request.onload = function () {
-
+    console.log(request)
+    if (request.status == 200) {
+        console.log(JSON.parse(request.response));
+    }
+    else {
+        console.log('error ${request.status} ${request.statusText}')
+        
+    }
 
 }
 
-request.send()
+request.send();
