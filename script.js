ceabb358285b9c0f6fb5a9bb391792ddc82c7f09
muckity <andrewgoldstein@hotmail.com>
2020-07-31 13:44:45 -0500

    update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var request = new XMLHttpRequest()
 
 request.open('GET', 'https://4lnixszdo4.execute-api.us-east-2.amazonaws.com/test/visitcount',true)    
-request.send()     
+    
 request.onload = function () {
-    var data = this.response
-    console.log(data)
+    var data = JSON.parse(this.response);
+    const p1 = document.createElement('p');
+    p1.textContent = console.log(data)    
 
 }
+request.send() 
