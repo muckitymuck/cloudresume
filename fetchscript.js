f04f58bacb5fade222646f4322911a883c9c7de2
muckity <andrewgoldstein@hotmail.com>
2020-07-31 13:26:22 -0500

    fetchscript update

--- fetchscript.js
+++ fetchscript.js
@@ -1,5 +1,8 @@
-const Url='https://4lnixszdo4.execute-api.us-east-2.amazonaws.com/test/visitcount';
+fetch('https://4lnixszdo4.execute-api.us-east-2.amazonaws.com/test/visitcount')
+.then(function (response) {
+    return response.json();
+})
+.then(function(myJson) {
+    console.log(JSON.stringify(myJson));
 
-fetch(Url)
-.then(data=>{return data.json()})
-.then(res=>{console.log(res)})
+})
--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 var request = new XMLHttpRequest()
 
-request.open('GET', 'https://4lnixszdo4.execute-api.us-east-2.amazonaws.com/test/visitcount')
-
-request.send()                    
+request.open('GET', 'https://4lnixszdo4.execute-api.us-east-2.amazonaws.com/test/visitcount',true)    
+request.send()     
 request.onload = function () {
-    var data = JSON.parse(this.response)
+    var data = this.response
     console.log(data)
 
 }
