de133597bd916d2dbc756b2bcfd6dd1563db051b
andre <andrewgoldstein@hotmail.com>
2020-08-01 06:33:30 -0500

    share

--- js/app.js
+++ js/app.js
@@ -1,26 +1,8 @@
-console.log(1+1)
-async function fetchText() {
-    let response = await fetch('https://4lnixszdo4.execute-api.us-east-2.amazonaws.com/test/visitcount');
-    let data = await response.text();
-    console.log(data);
+
+document.getElementById('fetchUserDataBtn').addEventListener('click', fetchUserData);
+
+function fetchUserData(){
+    fetch('https://jsonplaceholder.typicode.com/users/1')
+        .then(response => response.json())
+        .then(json => console.log(json))
 }
-async function getCount() {
-    let url = 'https://4lnixszdo4.execute-api.us-east-2.amazonaws.com/test/visitcount';
-    try {
-        let res = await fetch(url);
-        return await res.json();
-    } catch (errror) {
-        console.log(error);
-    }
-}
-async function renderCount() {
-    let count = await getCount();
-    let html = '';
-    count.map(count => {
-        let htmlSegment = '<div class="Visits"><p>ok+${console.log(data)}</p></div>';
-        html += htmlSegment;
-    });
-    let container = document.querySelector('.container');
-    container.innerHTML = html;
-}
-renderCount();
